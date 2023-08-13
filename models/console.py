1e5dae1663c4f504afb838e0f38291825075e00c
Ismael-ux-1 <ismaelabdulahi1@gmail.com>
2023-08-13 08:37:02 -0700

    Console 0.1

--- models/console.py
+++ models/console.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-"""Module for HBNBCommand class."""
 import cmd
-import models
-from models.base_model import BaseModel
 from models import storage
-
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
@@ -102,7 +99,6 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         pass
-
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
